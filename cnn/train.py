b214bf55800c87df030e3c98e688a439e80206b3
Chloe Pou-Prom <PouPromC@SMH.CA>
2019-07-22 17:45:31 -0400

    Add physionet scores

--- cnn/train.py
+++ cnn/train.py
@@ -63,6 +63,65 @@ def evaluate_model(model, dataloader):
         "PredictedProbability": predictions
     })
     return results.sort_values(["id", "ICULOS"])
+def compute_metrics(results):
+    auroc, auprc = compute_auc(labels=results.SepsisLabel,
+                               predictions=results.PredictedProbability)
+    accuracy, f_measure = compute_accuracy_f_measure(labels=results.SepsisLabel,
+                                                     predictions=results.Prediction)
+
+
+    # Compute utility.
+    num_files = results.id.nunique()
+    ids = results.id.unique()
+    observed_utilities = np.zeros(num_files)
+    best_utilities     = np.zeros(num_files)
+    worst_utilities    = np.zeros(num_files)
+    inaction_utilities = np.zeros(num_files)
+
+    dt_early   = -12
+    dt_optimal = -6
+    dt_late    = 3
+
+    max_u_tp = 1
+    min_u_fn = -2
+    u_fp     = -0.05
+    u_tn     = 0
+    for k in range(num_files):
+        patient_id = ids[k]
+        # labels = cohort_labels[k]
+        labels = results[results.id == patient_id].SepsisLabel.values
+        num_rows          = len(labels)
+        # observed_predictions = cohort_predictions[k]
+        observed_predictions = results[results.id == patient_id].Prediction.values
+        best_predictions     = np.zeros(num_rows)
+        worst_predictions    = np.zeros(num_rows)
+        inaction_predictions = np.zeros(num_rows)
+
+        if np.any(labels):
+            t_sepsis = np.argmax(labels) - dt_optimal
+            best_predictions[max(0, t_sepsis + dt_early) : min(t_sepsis + dt_late + 1, num_rows)] = 1
+        worst_predictions = 1 - best_predictions
+
+        observed_utilities[k] = compute_prediction_utility(labels, observed_predictions, dt_early, dt_optimal, dt_late, max_u_tp, min_u_fn, u_fp, u_tn)
+        best_utilities[k]     = compute_prediction_utility(labels, best_predictions, dt_early, dt_optimal, dt_late, max_u_tp, min_u_fn, u_fp, u_tn)
+        worst_utilities[k]    = compute_prediction_utility(labels, worst_predictions, dt_early, dt_optimal, dt_late, max_u_tp, min_u_fn, u_fp, u_tn)
+        inaction_utilities[k] = compute_prediction_utility(labels, inaction_predictions, dt_early, dt_optimal, dt_late, max_u_tp, min_u_fn, u_fp, u_tn)
+
+    unnormalized_observed_utility = np.sum(observed_utilities)
+    unnormalized_best_utility     = np.sum(best_utilities)
+    unnormalized_worst_utility    = np.sum(worst_utilities)
+    unnormalized_inaction_utility = np.sum(inaction_utilities)
+
+    normalized_observed_utility = (unnormalized_observed_utility - unnormalized_inaction_utility) / (unnormalized_best_utility - unnormalized_inaction_utility)
+
+    return {
+        "auroc": auroc,
+        "auprc": auprc,
+        "accuracy": accuracy,
+        "f_measure": f_measure,
+        "normalized_observed_utility": normalized_observed_utility,
+        "unnormalized_observed_utility": unnormalized_best_utility
+    }
 
 
 if __name__ == "__main__":
