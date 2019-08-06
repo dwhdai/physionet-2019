89a895ccbaa0d376a299b3f189013b5a113ffdfd
vivelagarfield <yangzh@smh.ca>
2019-08-06 13:33:32 -0400

    Uploaded best CNN model and minor change to evaluation file
    Best CNN model physionet_cnn_ratio_0_1.h5

--- cnn/train_from_intermediate.py
+++ cnn/train_from_intermediate.py
@@ -1,11 +1,10 @@
 # GPU
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "" # Pick which GPU to use "0-7"
-
 # General
 import pandas as pd
 import numpy as np
-
+from sklearn.metrics import classification_report
 # Keras
 import keras
 import keras.backend as k
@@ -15,15 +14,9 @@ from keras.models import Model, load_model
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 # Hyperparameters
-batch_size = 256
+batch_size = 1024
 num_epoch = 1000
-class_weight = {0:1, 1:55} # TODO: Needs calculation
-
-# Data Directory (Training & Test files are pre-saved numpy matrices)
-output_dir = "/home/yangzh/Desktop/output_dir/"
-train_file = "/data/physionet_data/splits/split_0/train_preprocessed_measured_window_24.npz"
-test_file = "/data/physionet_data/splits/split_0/valid_preprocessed_measured_window_24.npz"
-model_file = "physionet_cnn.h5"
+class_weight = {0:1, 1:10} # TODO: Needs calculation
 
 
 def load_data(train_file, test_file):
@@ -60,6 +53,12 @@ def load_data(train_file, test_file):
 
 def build_cnn(input_shape):
 
+    '''
+    Build the structure of CNN model
+    Input: input_shape: (window_size, feature_size, 1)
+    Returns: model ready for training process
+    '''
+
     # Build keras CNN model
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',kernel_initializer='he_normal',input_shape=input_shape))
@@ -86,8 +85,15 @@ def build_cnn(input_shape):
 
     return model
 
-def train_model(model, X_train, y_train, X_test, y_test, batch_size, num_epoch, callbacks, class_weight, model_file):
+def train_model(model, X_train, y_train, X_test, y_test, batch_size, num_epoch, class_weight, model_file):
     
+    '''
+    Model training
+    Input: Data: X_train, y_train, X_test, y_test
+           Hyperparameters: batch_size, num_epoch, class weight
+    
+    Returns: Trained and saved model
+    '''
     # Early stopping
     callbacks = [EarlyStopping(monitor = 'val_acc', patience = 20), 
                  ModelCheckpoint(model_file, save_best_only = True, monitor = 'val_acc')]
@@ -110,11 +116,16 @@ def train_model(model, X_train, y_train, X_test, y_test, batch_size, num_epoch, 
 
 def save_results(model_file, npz_file, output_dir):
     
+    '''
+    Print simple evaluation results of the model and save predictions in physionet format
+    Input: model_file, test_data file, output_dir
+    '''
+    
     # Load model
     model = load_model(model_file)
-
-    X_test = npz_file['valid_features']
-    y_test = npz_file['valid_outcomes']
+    npz = np.load(npz_file)
+    X_test = npz['valid_features']
+    y_test = npz['valid_outcomes']
 
     window_size = X_test.shape[1] # TODO: Change to args.window_size?
     feature_size = X_test.shape[2] # TODO: Change to feature_length
@@ -133,7 +144,7 @@ def save_results(model_file, npz_file, output_dir):
         else:
             y_pred_class.append(0)
     
-    print(classification_report)
+    print("Classification report:\n", classification_report(y_test, y_pred_class))
 
     # Save to psv in output_dir
     filenames = [str(f)[2:-1] + "v" for f in npz_file["valid_filenames"]]
@@ -148,3 +159,22 @@ def save_results(model_file, npz_file, output_dir):
         df.to_csv(filename_, index = False, sep = "|")
 
     return
+
+if __name__ == "__main__":
+    
+    # Train CNN model from intermediate data files (.npz)
+    '''
+    X_train, y_train, X_test, y_test, input_shape = load_data(train_file, test_file)
+    model = build_cnn(input_shape)
+    model = train_model(model, X_train, y_train, X_test, y_test, batch_size, num_epoch, class_weight, model_file)
+    save_results(model_file, test_file, output_dir)
+    
+    '''
+
+    # Load exisiting model and save results
+    output_dir = r"C:\Users\YangZh\Desktop\output_dir"
+    train_file = r"Z:\LKS-CHART\Projects\physionet_sepsis_project\data\splits\split_0\train_preprocessed_measured_window_24.npz"
+    test_file = r"Z:\LKS-CHART\Projects\physionet_sepsis_project\data\splits\split_0\valid_preprocessed_measured_window_24.npz"
+    model_file = r"C:\Users\YangZh\Desktop\physionet-2019\cnn\physionet_cnn_ratio_0_1.h5"
+
+    save_results(model_file, test_file, output_dir)
