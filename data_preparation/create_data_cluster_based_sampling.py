c4544d03e9d3d37083e12f380b001eb3e914186f
Chloe Pou-Prom <PouPromC@SMH.SMHROOT.NET>
2019-08-13 09:44:15 -0400

    Add file for kmeans sampling

--- data_preparation/create_data_cluster_based_sampling.py
+++ data_preparation/create_data_cluster_based_sampling.py
@@ -2,7 +2,7 @@
 # @Author: Chloe
 # @Date:   2019-07-24 16:57:51
 # @Last Modified by:   Chloe
-# @Last Modified time: 2019-08-07 16:25:09
+# @Last Modified time: 2019-08-08 10:05:35
 
 import argparse
 import numpy as np
@@ -12,6 +12,7 @@ import time
 from dataset import PhysionetDatasetCNN, FEATURES, LABEL, LABS_VITALS
 
 ratio = 0.1
+num_clusters = 5
 if __name__ == "__main__":
     start_time = time.time()
     argparser = argparse.ArgumentParser()
@@ -25,6 +26,8 @@ if __name__ == "__main__":
                            help="""Possible values:
                            - measured (forward-fill, add indicator variables, normalize, impute with -1),
                            - simple (only do forward-fill and impute with -1) """)
+    argparser.add_argument("--kmeans_train_file",
+                           default="Z:/LKS-CHART/Projects/physionet_sepsis_project/data/splits/split_0/Kmeans_clusters.csv")
     args = argparser.parse_args()
     print(args)
 
@@ -38,23 +41,32 @@ if __name__ == "__main__":
     train_dataset.__preprocess__(method=args.preprocessing_method)
     train_dataset.__setwindow__(window_size)
     print(train_dataset.data.columns)
-    print("Time elapsed since start: {}".format(time.time() - start_time))
-    print("Loading valid data")
-    valid_dataset = PhysionetDatasetCNN(args.valid_dir)
-    print("Time elapsed since start: {}".format(time.time() - start_time))
-    print("Preprocessing valid data")
-    valid_dataset.__preprocess__(method=args.preprocessing_method)
-    valid_dataset.__setwindow__(window_size)
-    print("Time elapsed since start: {}".format(time.time() - start_time))
 
     print("Save preprocessed datasets")
     print("Generating train data with ratio {}".format(ratio))
 
+    # Cluster-based sampling on train data 5x
     for r in range(5):
-        if ratio:
-            indices_no_outcome_keep = np.random.permutation(train_dataset.indices_no_outcome)[:int(1 / ratio) * len(train_dataset.indices_outcome)]
-            indices_train = np.random.permutation(np.concatenate((train_dataset.indices_outcome, indices_no_outcome_keep)))
-            train_n = len(indices_train)
+
+        data_no_outcome = train_dataset.data.iloc[train_dataset.indices_no_outcome].reset_index() # Keep index
+        kmeans = pd.read_csv(args.kmeans_train_file)
+
+        merged = data_no_outcome.merge(kmeans, on=["id", "ICULOS"])
+
+        num_keep = int(1 / ratio) * len(train_dataset.indices_outcome)
+        num_keep_per_cluster = int(num_keep / num_clusters)
+
+        indices_keep = []
+        for k in range(5):
+            indices_cluster = merged[merged["cluster_label"] == k]["index"].values
+            if len(indices_cluster) > 0:
+                indices_keep += np.random.permutation(indices_cluster)[:num_keep_per_cluster].tolist()
+
+
+        indices_train = np.random.permutation(np.concatenate((train_dataset.indices_outcome, indices_keep)))
+        train_n = len(indices_train)
+        print(len(indices_keep))
+        print(len(indices_train))
         train_features = np.zeros((train_n, window_size, num_features))
         train_outcomes = np.zeros((train_n, 1))
         train_ids = np.zeros((train_n, 1))
@@ -71,7 +83,7 @@ if __name__ == "__main__":
 
         if ratio:
             train_filename = os.path.join(args.output_dir,
-                                      "train_preprocessed_{}_window_{}_ratio_{}_{}.npz".format(args.preprocessing_method, window_size, str(ratio).replace(".", "_"), r))
+                                      "train_preprocessed_cluster_{}_window_{}_ratio_{}_{}.npz".format(args.preprocessing_method, window_size, str(ratio).replace(".", "_"), r))
         np.savez(train_filename,
                  train_features=train_features,
                  train_outcomes=train_outcomes,
@@ -80,27 +92,3 @@ if __name__ == "__main__":
                  train_filenames=train_filenames)
         print("Time elapsed since start: {}".format(time.time() - start_time))
 
-    print("Generating valid dataset")
-    valid_n = valid_dataset.__len__()
-    valid_features = np.zeros((valid_n, window_size, num_features))
-    valid_outcomes = np.zeros((valid_n, 1))
-    valid_ids = np.zeros((valid_n, 1))
-    valid_iculos = np.zeros((valid_n, 1))
-    valid_filenames = np.empty((valid_n), dtype="S10")
-    for i in range(valid_n):
-        item = valid_dataset.__getitem__(i)
-        valid_features[i, :, :] = item[0]
-        valid_outcomes[i, :] = item[1]
-        valid_ids[i] = item[2]
-        valid_iculos[i] = item[3]
-        valid_filenames[i] = item[4]
-
-    valid_filename = os.path.join(args.output_dir,
-                                  "valid_preprocessed_{}_window_{}.npz".format(args.preprocessing_method, window_size))
-    np.savez(valid_filename,
-             valid_features=valid_features,
-             valid_outcomes=valid_outcomes,
-             valid_ids=valid_ids,
-             valid_iculos=valid_iculos,
-             valid_filenames=valid_filenames)
-    print("Time elapsed since start: {}".format(time.time() - start_time))
